316628229638e09c1b2baadf2b5522083775c889
Dhruv <dhruvgoyal014@gmail.com>
2025-07-10 21:38:44 +0530

    bug fix in create transaction page

--- app/(main)/transaction/create/page.jsx
+++ app/(main)/transaction/create/page.jsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { getUserAccounts } from '@/actions/dashboard'
 import { defaultCategories } from '@/data/categories';
 import AddTransactionForm from '@/components/AddTransactions/AddTransactionForm'
