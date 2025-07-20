f858400985edda5e777602553a13afc2e8317454
Dhruv <dhruvgoyal014@gmail.com>
2025-07-20 11:17:17 +0530

    landing page updated

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="mt-40">
       <HeroSection />
 
-      <section className="py-20 bg-[var(--primaryColor-light)]">
+      {/* <section className="py-20 bg-[var(--primaryColor-light)]">
         <div className="container mx-auto px-4">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
             {statsData?.map((stat) => (
@@ -21,7 +21,7 @@ export default function Home() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section className="py-20">
         <div className="container mx-auto px-4">
